389d982413a352a6d0d85eae0da6ce46c04be44e
Pratham271 <chauhanpratham22@gmail.com>
2024-06-09 06:05:23 +0530

    added the room basic setup and made some changes in backend

--- Week23/omegle-clone/backend/src/managers/RoomManager.ts
+++ Week23/omegle-clone/backend/src/managers/RoomManager.ts
@@ -30,14 +30,16 @@ export class RoomManager {
     onOffer(roomId: string, sdp: string){
         const user2 = this.rooms.get(roomId)?.user2;
         user2?.socket.emit("offer", {
-            sdp
+            sdp,
+            roomId
         })
     }
 
     onAnswer(roomId: string, sdp: string){
         const user1 = this.rooms.get(roomId)?.user1;
         user1?.socket.emit("answer", {
-            sdp
+            sdp,
+            roomId
         })
     }
 
--- Week23/omegle-clone/frontend/src/components/Room.tsx
+++ Week23/omegle-clone/frontend/src/components/Room.tsx
@@ -1,12 +1,14 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useSearchParams } from "react-router-dom"
 
 const Room = () => {
   const [searchParams, setSearchParams] = useSearchParams()
+  const [socket, setSocket] = useState<WebSocket | null>(null)
   const name = searchParams.get("name")
 
   useEffect(()=> {
-
+    const socket = new WebSocket('ws://localhost:8080')
+    setSocket(socket)
   },[name])
   
   return (
